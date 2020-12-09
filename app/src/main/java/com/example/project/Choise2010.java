76a9f550058590ae07d76a79d50eed2c8dc269b3
Giacomo.Petta <giacomo.petta@studio.unibo.it>
2020-12-09 12:05:34 +0100

    versione 1.2

---
+++ app/src/main/java/com/example/project/Choise2010.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Choise2010 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choise2010);
+    }
+}
