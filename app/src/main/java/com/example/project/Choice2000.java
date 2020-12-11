54294e4a7b0e5a7487daf7af963e70972cb0d01e
Paulaner99 <rikipaolini99@gmail.com>
2020-12-11 15:08:14 +0100

    Versione 1.2

--- app/src/main/java/com/example/project/Choice2000.java
+++ app/src/main/java/com/example/project/Choice2000.java
@@ -10,14 +10,14 @@ import android.widget.TextView;
 
 public class Choice2000 extends AppCompatActivity {
 
-    private TextView cash;
+    private TextView text_cash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice2000);
 
-        cash = (TextView) findViewById(R.id.cash_value_text_view);
+        text_cash = (TextView) findViewById(R.id.cash_value_text_view);
     }
 
     public void choice2005(View view) {
@@ -27,6 +27,7 @@ public class Choice2000 extends AppCompatActivity {
 
     public void onRadioButtonClicked1(View view) {
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group1);
+        float value_cash=Float.parseFloat(text_cash.getText().toString());
         int id = radioGroup.getCheckedRadioButtonId();
         switch(id) {
             case R.id.radio_1_1:
@@ -35,12 +36,15 @@ public class Choice2000 extends AppCompatActivity {
                 break;
             case R.id.radio_1_2:
                 // 50%
+
                 break;
             case R.id.radio_1_3:
                 // 75%
+
                 break;
             case R.id.radio_1_4:
                 // 100%
+
                 break;
         }
     }
