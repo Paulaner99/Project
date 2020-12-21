a7d87ad8e59fd2c4d7ef87747e3102c0289fd4f3
Paulaner99 <rikipaolini99@gmail.com>
2020-12-21 17:46:27 +0100

    Versione 1.4.1

--- app/src/main/java/com/example/project/Choice2000.java
+++ app/src/main/java/com/example/project/Choice2000.java
@@ -13,14 +13,12 @@ import android.widget.Toast;
 
 public class Choice2000 extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.project.MESSAGE";
-    private TextView text_cash;
-    double guadagno = 0.0;
+    public static final String EXTRA_MESSAGE = "com.example.daniele.project.MESSAGE";
+    TextView text_cash;
+    RadioGroup radioGroup1, radioGroup2, radioGroup3, radioGroup4;
+    double guadagno;
     double cash;
     double value_cash;
-    RadioGroup radioGroup1;
-    RadioButton radioPerc_button;
-    Button btnDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +26,23 @@ public class Choice2000 extends AppCompatActivity {
         setContentView(R.layout.activity_choice2000);
 
         text_cash = (TextView) findViewById(R.id.cash_value_text_view);
-        value_cash = Float.parseFloat(text_cash.getText().toString());
+        radioGroup1 = (RadioGroup) findViewById(R.id.radio_group1);
+        radioGroup2 = (RadioGroup) findViewById(R.id.radio_group2);
+        radioGroup3 = (RadioGroup) findViewById(R.id.radio_group3);
+        radioGroup4 = (RadioGroup) findViewById(R.id.radio_group4);
+        value_cash = Double.parseDouble(text_cash.getText().toString());
         cash = value_cash;
-
+        guadagno = 0.0;
     }
 
 
     public void choice2005(View view) {
-        Intent intent = new Intent(this, Choise2010.class);
-        startActivity(intent);
+        double sum = 0.0;
 
-        intent.putExtra(EXTRA_MESSAGE, String.valueOf(guadagno + cash));
+        sum = guadagno + cash;
+        Intent intent = new Intent(this, Choise2010.class);
+        intent.putExtra(EXTRA_MESSAGE, String.valueOf(sum));
+        startActivity(intent);
 
     }
 
@@ -49,6 +53,12 @@ public class Choice2000 extends AppCompatActivity {
 
         // Check which radio button was clicked
         switch(view.getId()) {
+            case R.id.radio_1_0:
+                if (checked)
+                    // 0%
+                    guadagno = guadagno + value_cash * 0 * (1 - 0.2337);
+                cash = cash - value_cash * 0;
+                break;
             case R.id.radio_1_1:
                 if (checked)
                     // 25%
@@ -79,136 +89,120 @@ public class Choice2000 extends AppCompatActivity {
 
 
     public void onRadioButtonClicked2(View view) {
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group1);
-        float value_cash=Float.parseFloat(text_cash.getText().toString());
-        float guadagno2= (float) 0.0;
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
-        int id = radioGroup.getCheckedRadioButtonId();
-        switch(id) {
-            case R.id.radio_1_1:
-                // 25%
-            {
-                guadagno2= (float) (cash*0.0875);
-                cash= (float) (cash-(cash*0.25));
-            }
-
-            break;
-            case R.id.radio_1_2:
-                // 50%
-            {
-                guadagno2= (float) (cash*0.175);
-                cash= (float) (cash-(cash*0.5));
-            }
-
-            break;
-            case R.id.radio_1_3:
-                // 75%
-            {
-                guadagno2= (float) (cash*0.2625);
-                cash= (float) (cash-(cash*0.75));
-            }
-
-            break;
-            case R.id.radio_1_4:
-                // 100%
-            {
-                guadagno2= (float) (cash*0.35);
-                cash= (float) (cash-cash);
-            }
-
-            break;
-            default:
-
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_2_0:
+                if (checked)
+                    // 0%
+                    guadagno = guadagno + value_cash * 0 * (1 - 0.6500);
+                    cash = cash - value_cash * 0;
+                break;
+            case R.id.radio_2_1:
+                if (checked)
+                    // 25%
+                    guadagno = guadagno + value_cash * 0.25 * (1 - 0.6500);
+                    cash = cash - value_cash * 0.25;
+                break;
+            case R.id.radio_2_2:
+                if (checked)
+                    // 50%
+                    guadagno = guadagno + value_cash * 0.5 * (1 - 0.6500);
+                    cash = cash - value_cash * 0.5;
+                break;
+            case R.id.radio_2_3:
+                if (checked)
+                    // 75%
+                    guadagno = guadagno + value_cash * 0.75 * (1 - 0.6500);
+                    cash = cash - value_cash * 0.75;
+                break;
+            case R.id.radio_2_4:
+                if (checked)
+                    // 100%
+                    guadagno = guadagno + value_cash * 1 * (1 - 0.6500);
+                    cash = cash - value_cash * 1;
+                break;
         }
-        guadagno=guadagno+guadagno2;
     }
+
     public void onRadioButtonClicked3(View view) {
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group1);
-        float value_cash=Float.parseFloat(text_cash.getText().toString());
-        float guadagno3= (float) 0.0;
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
-        int id = radioGroup.getCheckedRadioButtonId();
-        switch(id) {
-            case R.id.radio_1_1:
-                // 25%
-            {
-                guadagno3= (float) (cash*0.48935);
-                cash= (float) (cash-(cash*0.25));
-            }
-
-            break;
-            case R.id.radio_1_2:
-                // 50%
-            {
-                guadagno3= (float) (cash*0.9787);
-                cash= (float) (cash-(cash*0.5));
-            }
-
-            break;
-            case R.id.radio_1_3:
-                // 75%
-            {
-                guadagno3= (float) (cash*1.46805);
-                cash= (float) (cash-(cash*0.75));
-            }
-
-            break;
-            case R.id.radio_1_4:
-                // 100%
-            {
-                guadagno3= (float) (cash*1.9574);
-                cash= (float) (cash-cash);
-            }
-
-            break;
-            default:
-
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_3_0:
+                if (checked)
+                    // 0%
+                    guadagno = guadagno + value_cash * 0 * (1 + 0.9574);
+                cash = cash - value_cash * 0;
+                break;
+            case R.id.radio_3_1:
+                if (checked)
+                    // 25%
+                    guadagno = guadagno + value_cash * 0.25 * (1 + 0.9574);
+                    cash = cash - value_cash * 0.25;
+                break;
+            case R.id.radio_3_2:
+                if (checked)
+                    // 50%
+                    guadagno = guadagno + value_cash * 0.5 * (1 + 0.9574);
+                    cash = cash - value_cash * 0.5;
+                break;
+            case R.id.radio_3_3:
+                if (checked)
+                    // 75%
+                    guadagno = guadagno + value_cash * 0.75 * (1 + 0.9574);
+                    cash = cash - value_cash * 0.75;
+                break;
+            case R.id.radio_3_4:
+                if (checked)
+                    // 100%
+                    guadagno = guadagno + value_cash * 1 * (1 + 0.9574);
+                    cash = cash - value_cash * 1;
+                break;
         }
-        guadagno=guadagno+guadagno3;
     }
+
     public void onRadioButtonClicked4(View view) {
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group1);
-        float value_cash=Float.parseFloat(text_cash.getText().toString());
-        float guadagno4= (float) 0.0;
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
-        int id = radioGroup.getCheckedRadioButtonId();
-        switch(id) {
-            case R.id.radio_1_1:
-                // 25%
-            {
-                guadagno4= (float) (cash*0.3224);
-                cash= (float) (cash-(cash*0.25));
-            }
-
-            break;
-            case R.id.radio_1_2:
-                // 50%
-            {
-                guadagno4= (float) (cash*0.6448);
-                cash= (float) (cash-(cash*0.5));
-            }
-
-            break;
-            case R.id.radio_1_3:
-                // 75%
-            {
-                guadagno4= (float) (cash*0.9672);
-                cash= (float) (cash-(cash*0.75));
-            }
-
-            break;
-            case R.id.radio_1_4:
-                // 100%
-            {
-                guadagno4= (float) (cash*1.2896);
-                cash= (float) (cash-cash);
-            }
-
-            break;
-            default:
-
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_4_0:
+                if (checked)
+                    // 0%
+                    guadagno = guadagno + value_cash * 0 * (1 + 0.2896);
+                    cash = cash - value_cash * 0;
+                break;
+            case R.id.radio_4_1:
+                if (checked)
+                    // 25%
+                    guadagno = guadagno + value_cash * 0.25 * (1 + 0.2896);
+                    cash = cash - value_cash * 0.25;
+                break;
+            case R.id.radio_4_2:
+                if (checked)
+                    // 50%
+                    guadagno = guadagno + value_cash * 0.5 * (1 + 0.2896);
+                    cash = cash - value_cash * 0.5;
+                break;
+            case R.id.radio_4_3:
+                if (checked)
+                    // 75%
+                    guadagno = guadagno + value_cash * 0.75 * (1 + 0.2896);
+                    cash = cash - value_cash * 0.75;
+                break;
+            case R.id.radio_4_4:
+                if (checked)
+                    // 100%
+                    guadagno = guadagno + value_cash * 1 * (1 + 0.2896);
+                    cash = cash - value_cash * 1;
+                break;
         }
-        guadagno=guadagno+guadagno4;
     }
 
 }
