53dac2870d10444d6c06a27f2f0ede144c356993
Paulaner99 <rikipaolini99@gmail.com>
2021-02-09 16:18:08 +0100

    Versione 2.3

--- app/src/main/java/com/example/project/Risultato.java
+++ app/src/main/java/com/example/project/Risultato.java
@@ -20,5 +20,17 @@ public class Risultato extends AppCompatActivity {
 
         TextView text_cash = findViewById(R.id.cash_value_finale);
         text_cash.setText(finalScore);
+
+        TextView final_result = findViewById(R.id.score);
+
+        if( Float.parseFloat(text_cash.getText().toString()) > 3800000){
+            final_result.setText(R.string.win);
+        } else { final_result.setText(R.string.lose); }
     }
+
+    public void Home(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
