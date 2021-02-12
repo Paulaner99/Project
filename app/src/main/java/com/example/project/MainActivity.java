d455c693715f16cc947f04eb0a2346657bd7eefd
GiacomoPetta <giacomo.petta99@hotmail.it>
2021-02-12 21:34:45 +0100

    Versione 2.4

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -6,20 +6,48 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    static final int READ_BLOCK_SIZE = 100;
+    TextView high_score;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MediaPlayer music= MediaPlayer.create(MainActivity.this,R.raw.music1);
         music.start();
+
+
+        //Lettura High-Score
+
+        high_score = findViewById(R.id.high_score_text_view);
+
+        try{
+            FileInputStream fileIn = openFileInput("file_buffet.txt");
+            InputStreamReader inputReader = new InputStreamReader(fileIn);
+
+            char[] inputBuffer = new char[READ_BLOCK_SIZE];
+            String s = "";
+            int charRead;
+
+            while ((charRead = inputReader.read(inputBuffer)) > 0) {
+                String readstring = String.copyValueOf(inputBuffer, 0, charRead);
+                s += readstring;
+            }
+            inputReader.close();
+            high_score.setText(s);
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
     public void instructions(View view){
         Intent intent = new Intent(this, Instructions.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/project/Risultato.java
+++ app/src/main/java/com/example/project/Risultato.java
@@ -2,12 +2,22 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Risultato extends AppCompatActivity {
+
+    static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +36,44 @@ public class Risultato extends AppCompatActivity {
         if( Float.parseFloat(text_cash.getText().toString()) > 3800000){
             final_result.setText(R.string.win);
         } else { final_result.setText(R.string.lose); }
+
+
+        //Salvataggio High-Score
+        String s = "";
+
+        try{
+            FileInputStream fileIn = openFileInput("file_buffet.txt");
+            InputStreamReader inputReader = new InputStreamReader(fileIn);
+
+            char[] inputBuffer = new char[READ_BLOCK_SIZE];
+            int charRead;
+
+            while ((charRead = inputReader.read(inputBuffer)) > 0) {
+                String readstring = String.copyValueOf(inputBuffer, 0, charRead);
+                s += readstring;
+            }
+            inputReader.close();
+        } catch (Exception e) { e.printStackTrace(); }
+
+        if( Float.parseFloat(text_cash.getText().toString()) > Float.parseFloat(s)) {
+
+            try {
+                FileOutputStream fileout = openFileOutput("file_buffet.txt", MODE_PRIVATE);
+                OutputStreamWriter outputWriter = new OutputStreamWriter(fileout);
+                outputWriter.write(text_cash.getText().toString());
+                outputWriter.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     public void Home(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 
 }
